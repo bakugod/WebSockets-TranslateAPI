f399d046a599156ce9e37372bfa0467b329c6313
bakugod <bakugod@yandex.ru>
2019-04-18 20:26:36 +0300

    New features
    Add new id system and now you can use this app with semi-normal UI and UX

--- dev.js
+++ dev.js
@@ -10,13 +10,31 @@ const webSocketServer = new WebSocketServer.Server({
   port: 8081
 });
 
-webSocketServer.on('connection', function(ws) {
+webSocketServer.on('connection', (ws) => {
 
-  const id = Math.random();
+  /*
+  * easiest method to create unique id
+  * i dont love this long UNIX string
+  * and i trim it to 9 symbols with ~
+  */
+  const uniqId = () =>{
+      const unixDate = new Date().getTime();
+      return Math.abs(~unixDate);
+    }
+
+  const id = uniqId();
   clients[id] = ws;
   console.log("новое соединение " + id);
 
-  ws.on('message', function(message) {
+    /*
+      & [key=<уникальный ключ>]
+      & [text=<переводимый текст>]
+      & [lang=<направление перевода>]
+      & [format=<формат текста>]
+      & [options=<опции перевода>]
+      & [callback=<имя callback-функции>]
+   */
+  ws.on('message', (message) => {
     unirest.post("https://translate.yandex.net/api/v1.5/tr.json/translate")
     .header("Content-Type", "application/x-www-form-urlencoded")
     .send("key=trnsl.1.1.20190417T190639Z.d307e3b265e55b08.3973b5db3a0a45ab579bd9ca519600303b52630f")
@@ -24,16 +42,17 @@ webSocketServer.on('connection', function(ws) {
     .send("lang=en-ru")
     .send("format=plain")
     .send("options=1")
-    .end(function (result) {
-      console.log('получено сообщение ' + message/*result.status, result.headers, */+ '\n перевод ' + result.body.text.toString());
-    });
-
-    for (let key in clients) {
-      clients[key].send(message);
+    .end((result) =>{
+      for (let key in clients) {
+      clients[key].send(result.body.text.toString() + ' от ' + id);
     }
+      console.log('получено сообщение ' + message + ' от ' + id);
+      console.log(/*result.status, result.headers, */); //More info here
+      console.log('\n перевод ' + result.body.text.toString());
+    });
   });
 
-  ws.on('close', function() {
+  ws.on('close', () => {
     console.log('соединение закрыто ' + id);
     delete clients[id];
   });
@@ -49,11 +68,4 @@ const app = express();
 
   
 
-		app.listen(8080);
-/*
-& [text=<переводимый текст>]
- & [lang=<направление перевода>]
- & [format=<формат текста>]
- & [options=<опции перевода>]
- & [callback=<имя callback-функции>]
- */
+	app.listen(8080);
